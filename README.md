12dad34101c1b9a9c5ed26e1176acbe8067e386b
Kris Verbeeck <kris.verbeeck@ingenico.com>
2022-12-01 12:36:55 +0100

    day1 (Rust)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # aoc-2022
 
 Solutions for [Advent of Code 2022](https://adventofcode.com/2022) in
-* tbd
+* Python
+* Rust
 
--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,13 +1,48 @@
-pub fn part1() -> String {
-    "world".to_string()
+use std::collections::BinaryHeap;
+
+use crate::util::load;
+
+pub fn input() -> Vec<String> {
+    let calories: Vec<String> = load("data/day1.txt");
+    calories
+}
+
+fn sorted_sums(calories: Vec<String>) -> Vec<u32> {
+    let (mut sums, last) = calories.iter().fold(
+        (BinaryHeap::new(), 0),
+        |(mut sums, current), cal| match cal.as_str() {
+            "" => {
+                sums.push(current);
+                (sums, 0)
+            }
+            s => (sums, current + s.parse::<u32>().unwrap()),
+        },
+    );
+    sums.push(last);
+    let mut v = sums.into_sorted_vec();
+    v.reverse();
+    v
+}
+
+pub fn part1(calories: Vec<String>) -> u32 {
+    sorted_sums(calories)[0]
+}
+
+pub fn part2(calories: Vec<String>) -> u32 {
+    sorted_sums(calories)[0..3].iter().sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day1::part1;
+    use crate::day1::{input, part1, part2};
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), "world");
+        assert_eq!(part1(input()), 72017);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(input()), 212520);
     }
 }

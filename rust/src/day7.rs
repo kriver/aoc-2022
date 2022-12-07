f90199e9a54b8b19ead594e919c1ea59a1a5a4f8
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2022-12-07 20:01:23 +0100

    day7 (rust, using slice/index, compile errors)

--- rust/src/day7.rs
+++ rust/src/day7.rs
@@ -14,45 +14,43 @@ fn add_to_folder(folder: &mut File, name: String, entry: File) {
     }
 }
 
-fn parse_lines(mut it: impl Iterator<Item = String>, folder: &mut File) {
+fn parse_lines(lines: &[String], mut i: usize, folder: &mut File) -> usize {
     loop {
-        match it.next() {
-            None => return,
-            Some(line) => {
-                let token: Vec<&str> = line.split(' ').collect();
-                match token[0] {
-                    "$" => match token[1] {
-                        "cd" => match token[2] {
-                            ".." => return,
-                            name => {
-                                if let File::Folder(ref mut map) = folder {
-                                    if let Some(ref mut f) = map.get(name) {
-                                        parse_lines(it, f);
-                                    }
-                                }
-                            }
-                        },
-                        "ls" => (), // nop
-                        _ => panic!("unexpected cmd"),
-                    },
-                    "dir" => {
-                        add_to_folder(folder, token[1].to_owned(), File::Folder(HashMap::new()))
-                    }
-                    sz => add_to_folder(
-                        folder,
-                        token[1].to_owned(),
-                        File::Plain(sz.parse::<usize>().unwrap()),
-                    ),
-                }
-            }
+        if i >= lines.len() {
+            return i;
         }
+        let line = &lines[i];
+        i += 1;
+        let token: Vec<&str> = line.split(' ').collect();
+        match token[0] {
+            "$" => match token[1] {
+                "cd" => match token[2] {
+                    ".." => return i,
+                    name => {
+                        if let File::Folder(ref mut map) = folder {
+                            if let Some(ref mut f) = map.get(name) {
+                                i = parse_lines(lines, i, f);
+                            }
+                        }
+                    }
+                },
+                "ls" => (), // nop
+                _ => panic!("unexpected cmd"),
+            },
+            "dir" => add_to_folder(folder, token[1].to_owned(), File::Folder(HashMap::new())),
+            sz => add_to_folder(
+                folder,
+                token[1].to_owned(),
+                File::Plain(sz.parse::<usize>().unwrap()),
+            ),
+        };
     }
 }
 
 fn parse_input() -> File {
     let lines: Vec<String> = load("data/day7.txt");
     let mut fs = File::Folder(HashMap::new());
-    parse_lines(lines.into_iter(), &mut fs);
+    parse_lines(&lines, 0, &mut fs);
     fs
 }
 

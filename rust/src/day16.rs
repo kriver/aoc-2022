cd0e9eb775a8cd5fd5ff762a9b106e82e0bc9c7d
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2022-12-18 12:36:18 +0100

    day16 (3rd try, 450s execution time :-/)

--- rust/src/day16.rs
+++ rust/src/day16.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 use crate::util::load;
 
@@ -79,9 +82,15 @@ impl Rooms {
         m
     }
 
-    fn pressure_for(&self, valves: &[String], distances: &Vec<Vec<u32>>) -> u32 {
-        let mut tl = 30; // time left
-        let mut pos = self.name2id["AA"];
+    fn pressure_for(
+        &self,
+        time_left: u32,
+        start: &str,
+        valves: &Vec<String>,
+        distances: &Vec<Vec<u32>>,
+    ) -> u32 {
+        let mut tl = time_left;
+        let mut pos = self.name2id[start];
         let mut pressure = 0;
         for v in valves {
             let next = self.name2id[v];
@@ -96,21 +105,27 @@ impl Rooms {
         pressure
     }
 
-    fn max_pressure(&mut self, distances: &Vec<Vec<u32>>) -> u32 {
+    fn max_pressure(
+        &self,
+        time_left: u32,
+        start: &str,
+        valves: &mut Vec<String>,
+        distances: &Vec<Vec<u32>>,
+    ) -> u32 {
         // loop over all permutations of rooms with valves (Heap's algorithm)
-        let n = self.valves.len();
+        let n = valves.len();
         let mut c = vec![0; n];
-        let mut max_pressure = self.pressure_for(&self.valves, distances);
+        let mut max_pressure = self.pressure_for(time_left, start, valves, distances);
         let mut i = 1;
         let mut cnt = 0;
         while i < n {
             if c[i] < i {
                 if i % 2 == 0 {
-                    self.valves.swap(0, i);
+                    valves.swap(0, i);
                 } else {
-                    self.valves.swap(c[i], i);
+                    valves.swap(c[i], i);
                 }
-                let pressure = self.pressure_for(&self.valves, distances);
+                let pressure = self.pressure_for(time_left, start, valves, distances);
                 max_pressure = max_pressure.max(pressure);
                 c[i] += 1;
                 i = 1;
@@ -120,17 +135,81 @@ impl Rooms {
             }
             cnt += 1;
             if cnt % 1000000 == 0 {
-                println!("{:8} - {:5} - {:?}", cnt, max_pressure, self.valves);
+                println!("{:8} - {:5} - {:?}", cnt, max_pressure, valves);
             }
         }
         max_pressure
     }
+
+    fn with_look_ahead(&self, look_ahead: usize) -> u32 {
+        let distances = self.distances();
+        let mut pressure = 0;
+        let mut visited: HashSet<String> = HashSet::new();
+        let mut tl = 30;
+        let mut prev = "AA".to_owned();
+        while self.valves.len() - visited.len() >= 4 {
+            let mut best_p = 0; // pressure
+            let mut best_n = None; // next
+            let mut best_d = 0; // delta
+            let mut iterators = vec![self.valves.iter(); look_ahead];
+            let mut valves = iterators
+                .iter_mut()
+                .map(|it| it.next().unwrap().to_owned())
+                .collect::<Vec<_>>();
+            'outer: loop {
+                let current: HashSet<String> = HashSet::from_iter(valves.iter().map(|v| v.clone()));
+                if current.len() == look_ahead && visited.intersection(&current).count() == 0 {
+                    // all different valves, and none yet visited
+                    let p = self.pressure_for(tl, &prev, &mut valves, &distances);
+                    if p > best_p {
+                        best_p = p;
+                        best_n = Some(valves[0].to_owned());
+                        best_d = distances[self.name2id[&prev]][self.name2id[&valves[0]]] + 1;
+                    }
+                }
+                for i in 0..look_ahead {
+                    match iterators[i].next() {
+                        Some(v) => {
+                            valves[i] = v.to_owned();
+                            break;
+                        }
+                        None => {
+                            if i == look_ahead - 1 {
+                                break 'outer;
+                            }
+                            iterators[i] = self.valves.iter();
+                            valves[i] = iterators[i].next().unwrap().to_owned();
+                        }
+                    }
+                }
+            }
+            match best_n {
+                None => break,
+                Some(n) => {
+                    prev = n.clone();
+                    tl -= best_d;
+                    println!("{:2} Best = {} with {}", tl, n, best_p);
+                    pressure += tl * self.rooms[self.name2id[&n]].rate;
+                    println!("   Total pressure now at {}", pressure);
+                    visited.insert(n);
+                    println!("   Visited {}/{}", visited.len(), self.valves.len());
+                }
+            }
+        }
+        let mut remaining: Vec<String> = HashSet::from_iter(self.valves.iter().map(|v| v.clone()))
+            .difference(&visited)
+            .map(|s| s.to_owned())
+            .collect();
+        println!("Remains {:?}", remaining);
+        let p = self.max_pressure(tl, &prev, &mut remaining, &distances);
+        println!("Remaining pressure is {}", p);
+        pressure + p
+    }
 }
 
 pub fn part1() -> u32 {
-    let mut rooms = Rooms::load("data/day16.txt");
-    let d = rooms.distances();
-    rooms.max_pressure(&d)
+    let rooms = Rooms::load("data/day16.txt");
+    rooms.with_look_ahead(6)
 }
 
 mod tests {
@@ -138,7 +217,7 @@ mod tests {
     fn test_part1() {
         let pressure = super::part1();
         println!("Pressure: {}", pressure);
-        assert_eq!(pressure, 0);
+        assert_eq!(pressure, 1789);
     }
 
     #[test]
